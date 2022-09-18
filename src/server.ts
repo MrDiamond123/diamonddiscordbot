159c7d621dc5fc7d8bea74639c108b7a671d497d
Mr_Diamond123 <kurtisiscool123@gmail.com>
2022-09-18 16:27:54 -0400

    ci stuff

--- src/server.ts
+++ src/server.ts
@@ -53,7 +53,7 @@ router.post('/', async (request, env) => {
           return new JsonResponse({
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
-              content: JSON.stringify(posts)
+              content: posts
             }
           })
         default:
@@ -68,7 +68,7 @@ router.post('/', async (request, env) => {
             return new JsonResponse({
               type: InteractionResponseType.APPLICATION_COMMAND_AUTOCOMPLETE_RESULT,
               data: {
-                choices: JSON.stringify(subreddits),
+                choices: subreddits,
               }
             })
       }
