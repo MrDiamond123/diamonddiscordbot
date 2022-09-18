601ba5aadfeb46a4b95f82327015cd64505f5397
Mr_Diamond123 <kurtisiscool123@gmail.com>
2022-09-18 18:47:32 -0400

    figure out how dumb i am

--- src/reddit.js
+++ src/reddit.js
@@ -9,7 +9,7 @@ export async function handleSubredditCommand(env, message) {
                 'content': "test"
             }
         })
-    console.log (await response.json())
+    console.dir(await response.json())
 }
    
 export async function getRedditURL(subreddit) {
